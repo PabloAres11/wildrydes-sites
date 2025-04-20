52de07134231c1a589cbf62706f2573a898c9fb3
PabloAres11 <160888980+PabloAres11@users.noreply.github.com>
2025-04-20 18:53:58 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'pabloares', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_HdtbErrON', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '1e9lotgv3mq037v47spn7eqn4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
