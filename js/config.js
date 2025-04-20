35a105401ecda730d858ed445f61d168021498c1
PabloAres11 <160888980+PabloAres11@users.noreply.github.com>
2025-04-20 18:46:36 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'pabloares', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1e9lotgv3mq037v47spn7eqn4a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
